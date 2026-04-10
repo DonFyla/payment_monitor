45caa2dcd7a9bf00bf9394bd6cdbd95e62266383
donfyla <alabioyetope@gmail.com>
2026-04-10 19:14:38 +0100

    Add automatic superuser creation for Render deployment
    - Create management command to create superuser from env vars
    - Update entrypoint.sh to run superuser creation on startup
    - Update render.yaml with superuser environment variables
    - Auto-generates random password for admin user

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Creating superuser if credentials provided..."
+python manage.py create_superuser
+
 echo "Starting Gunicorn..."
 exec gunicorn chess_academy.wsgi:application --bind 0.0.0.0:$PORT --workers 4 --threads 2
